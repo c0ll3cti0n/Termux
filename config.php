af827e45ac446b6e0c522fa2a1aabfd9092f9f27
c0ll3cti0n <16981914+c0ll3cti0n@users.noreply.github.com>
2019-12-26 00:08:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,2 +1,4 @@
-<?php
-$Wallet_Address = "0xd94de17983f238c9b32bbd97b15a82d85db8942f";
+<? /* Tool được cover bởi Kênh Youtube Kiếm tiền Du học Vlog |nghiêm cấm sao chép| */
+
+$email = "tajibaevaan@gmail.com";
+$pass = "534029an";
